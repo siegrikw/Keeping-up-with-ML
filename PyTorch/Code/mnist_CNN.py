af40c32ff5770412c3b296a1722e2925a20d0cc6
siegrikw <40180950+siegrikw@users.noreply.github.com>
2020-11-05 13:58:20 -0500

    gpu_cnn

--- PyTorch/Code/mnist_CNN.py
+++ PyTorch/Code/mnist_CNN.py
@@ -15,6 +15,9 @@ import requests
 import pickle
 import gzip
 
+global CUDA
+CUDA = torch.cuda.is_available()
+
 class Mnist_CNN(nn.Module):
 
     def __init__(self):
@@ -86,16 +89,43 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
     for epoch in range(1,epochs+1):
         for xb,yb in train_dl:
             # set_trace() -> Slows the code immensely do not use unless absolutely needed
-            batch_loss(model,loss_func,xb,yb,opt)
+            if CUDA:
+                batch_loss(model,loss_func,xb.cuda(),yb.cuda(),opt)
+            else:
+                batch_loss(model,loss_func,xb,yb,opt)
 
         model.eval()
         with torch.no_grad():
-            losses,nums = zip(
-            *[batch_loss(model, loss_func,xb,yb) for xb,yb in valid_dl]
-            )
+            if CUDA:
+                losses,nums = zip(
+                *[batch_loss(model, loss_func,xb.cuda(),yb.cuda()) for xb,yb in valid_dl]
+                )
+            else:
+                losses,nums = zip(
+                *[batch_loss(model, loss_func,xb.cuda(),yb.cuda()) for xb,yb in valid_dl]
+                )
+
         valid_loss = np.sum(np.multiply(losses,nums)) / np.sum(nums)
 
         print(f'Epoch: {epoch} validation_loss: {valid_loss}')
+
+def get_model():
+
+    if CUDA:
+        model = Mnist_CNN().cuda()
+        print(f'USE GPU: {CUDA}')
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,4), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,4), 'GB')
+
+
+    else:
+        model = Mnist_CNN()
+
+    opt = optim.SGD(model.parameters(),lr=lr,momentum=0.9)
+
+    return model,opt
 
 x_train,x_test,y_train,y_test = map(torch.tensor,fetch_data())
 display_digit(digit=x_train[0])
@@ -107,16 +137,20 @@ batch_size = 64 #Size of training batches
 lr = 0.1  # learning rate
 epochs = 10  # how many epochs to train for
 
+
 #Create the pytorch data set object
 train_ds,valid_ds = get_dataset(x_train,x_test,y_train,y_test)
 #Create a data loader object from the data set object
 train_dl,valid_dl = get_dataloader(train_ds,valid_ds,batch_size=batch_size)
-#Create the simple FF model
-model = Mnist_CNN()
-#Select an optimizer for propagate error
-opt = optim.SGD(model.parameters(),lr=lr,momentum=0.9)
+
+#Get the model and the optimizer for the model
+model,opt = get_model()
+
 #Select a loss function
 loss_func = F.cross_entropy
 
+import time
+start = time.time()
 #Train the Model
 fit(epochs, model, loss_func, opt, train_dl, valid_dl)
+print(f"Training time: {time.time()-start}")
